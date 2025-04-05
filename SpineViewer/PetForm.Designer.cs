580eaf990ddae7d5c9638d0e5af762cdbf9bd937
ww-rm <ww-rm@qq.com>
2025-04-05 11:57:44 +0800

    测试代码

---
+++ SpineViewer/PetForm.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace SpineViewer
+{
+    partial class PetForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // PetForm
+            // 
+            AutoScaleMode = AutoScaleMode.None;
+            ClientSize = new Size(490, 456);
+            ControlBox = false;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "PetForm";
+            ShowIcon = false;
+            ShowInTaskbar = false;
+            StartPosition = FormStartPosition.Manual;
+            Text = "PetForm";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ SpineViewer/PetForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpineViewer
+{
+    public partial class PetForm: Form
+    {
+        public PetForm()
+        {
+            InitializeComponent();
+        }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                //var style = Win32.GetWindowLong(hWnd, Win32.GWL_STYLE) | Win32.WS_POPUP;
+                //var exStyle = Win32.GetWindowLong(hWnd, Win32.GWL_EXSTYLE) | Win32.WS_EX_LAYERED | Win32.WS_EX_TOOLWINDOW | Win32.WS_EX_TOPMOST;
+                //Win32.SetWindowLong(hWnd, Win32.GWL_STYLE, style);
+                //Win32.SetWindowLong(hWnd, Win32.GWL_EXSTYLE, exStyle);
+                //Win32.SetLayeredWindowAttributes(hWnd, crKey, 255, Win32.LWA_COLORKEY | Win32.LWA_ALPHA);
+                //Win32.SetWindowPos(hWnd, Win32.HWND_TOPMOST, 0, 0, 0, 0, Win32.SWP_NOMOVE | Win32.SWP_NOSIZE);
+                var cp = base.CreateParams;
+                cp.ExStyle = Win32.WS_EX_LAYERED | Win32.WS_EX_TOPMOST;
+                cp.Style = Win32.WS_POPUP;
+                //cp.ExStyle |= Win32.WS_EX_LAYERED | Win32.WS_EX_TOOLWINDOW | Win32.WS_EX_TOPMOST;
+                return cp;
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            ;
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            ;
+        }
+    }
+}
