915535507f123f0fd55a3fefe3ff8d7622b978ef
ww-rm <ww-rm@qq.com>
2026-01-24 23:31:03 +0800

    增加psd格式导出

---
+++ Spine/Exporters/PsdExporter.cs
@@ -0,0 +1,62 @@
+﻿using PsdWriter;
+using SFML.System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spine.Exporters
+{
+    public class PsdExporter : BaseExporter
+    {
+        public PsdExporter(uint width = 100, uint height = 100) : base(width, height) { }
+        public PsdExporter(Vector2u resolution) : base(resolution) { }
+
+        public sealed override void Export(string output, params SpineObject[] spines) => Export(output, default, spines);
+
+        /// <summary>
+        /// 导出给定的模型, 从前往后对应从上往下的渲染顺序
+        /// </summary>
+        /// <param name="output">Psd 文件输出路径</param>
+        /// <param name="ct">取消令牌</param>
+        /// <param name="spines">要导出的模型, 从前往后对应从上往下的渲染顺序</param>
+        public void Export(string output, CancellationToken ct, params SpineObject[] spines)
+        {
+            var resolution = _renderTexture.Size;
+            var psdWriter = new PsdWriter.PsdWriter(resolution.X, resolution.Y);
+            _renderTexture.SetActive(true);
+
+            var layerCount = spines.Select(sp => sp.IterDrawCount).Sum();
+            var layerIdx = 0;
+
+            _progressReporter?.Invoke(layerCount, 0, $"[0/{layerCount}] {output}");
+            foreach (var sp in spines.Reverse())
+            {
+                if (ct.IsCancellationRequested)
+                {
+                    _logger.Info("Export cancelled");
+                    break;
+                }
+
+                psdWriter.BeginGroup(sp.Name);
+                foreach (var (name, image) in sp.IterDraw(_renderTexture))
+                {
+                    if (ct.IsCancellationRequested)
+                    {
+                        image.Dispose();
+                        break;
+                    }
+                    _progressReporter?.Invoke(layerCount, layerIdx + 1, $"[{layerIdx + 1}/{layerCount}] {output} <{sp.Name}/{name}>");
+                    psdWriter.AddRgbaLayer(image.Pixels, name, true);
+                    image.Dispose();
+                    layerIdx++;
+                }
+                psdWriter.EndGroup();
+            }
+
+            _renderTexture.SetActive(false);
+            psdWriter.WriteTo(output);
+        }
+    }
+}
---
+++ SpineViewer/ViewModels/Exporters/PsdExporterViewModel.cs
@@ -0,0 +1,120 @@
+﻿using Spine;
+using Spine.Exporters;
+using SpineViewer.Models;
+using SpineViewer.Resources;
+using SpineViewer.Services;
+using SpineViewer.ViewModels.MainWindow;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace SpineViewer.ViewModels.Exporters
+{
+    public class PsdExporterViewModel : BaseExporterViewModel
+    {
+        public PsdExporterViewModel(MainWindowViewModel vmMain) : base(vmMain)
+        {
+            // PSD 文件背景颜色固定为透明, 且不允许用户在面板修改
+            _backgroundColor = Color.FromArgb(0, 0, 0, 0);
+        }
+
+        protected override void Export(SpineObjectModel[] models)
+        {
+            base.Export(models);
+            if (!DialogService.ShowPsdExporterDialog(this)) return;
+            SpineObject[] spines = models.Select(m => m.GetSpineObject(true)).ToArray();
+            ProgressService.RunAsync((pr, ct) => ExportTask(spines, pr, ct), AppResource.Str_PsdExporterTitle);
+            foreach (var sp in spines) sp.Dispose();
+        }
+
+        private void ExportTask(SpineObject[] spines, IProgressReporter pr, CancellationToken ct)
+        {
+            if (spines.Length <= 0) return;
+
+            var timestamp = DateTime.Now.ToString("yyMMddHHmmss");
+            using var exporter = new PsdExporter(_renderer.Resolution.X + _margin * 2, _renderer.Resolution.Y + _margin * 2);
+
+            // 非自动分辨率则直接用预览画面的视区参数
+            if (!_autoResolution)
+            {
+                using var view = _renderer.GetView();
+                var bounds = view.GetBounds().GetCanvasBounds(_renderer.Resolution, _margin);
+                exporter.Size = bounds.Size;
+                exporter.Center = bounds.Position + bounds.Size / 2;
+                exporter.Rotation = view.Rotation;
+            }
+
+            if (_exportSingle)
+            {
+                var filename = $"layers_{timestamp}_{Guid.NewGuid().ToString()[..6]}.psd";
+                var output = Path.Combine(_outputDir!, filename);
+
+                if (_autoResolution) SetAutoResolutionStatic(exporter, spines);
+
+                exporter.ProgressReporter = (total, done, text) =>
+                {
+                    pr.Total = total;
+                    pr.Done = done;
+                    pr.ProgressText = text;
+                    _vmMain.ProgressValue = pr.Done / pr.Total;
+                };
+
+                _vmMain.ProgressState = System.Windows.Shell.TaskbarItemProgressState.Normal;
+                _vmMain.ProgressValue = 0;
+                try
+                {
+                    exporter.Export(output, ct, spines);
+                }
+                catch (Exception ex)
+                {
+                    _logger.Debug(ex.ToString());
+                    _logger.Error("Failed to export {0}, {1}", output, ex.Message);
+                }
+                _vmMain.ProgressState = System.Windows.Shell.TaskbarItemProgressState.None;
+            }
+            else
+            {
+                pr.Total = spines.Select(sp => sp.IterDrawCount).Sum();
+                pr.Done = 0;
+
+                exporter.ProgressReporter = (total, done, text) =>
+                {
+                    pr.Done++;
+                    pr.ProgressText = text;
+                    _vmMain.ProgressValue = pr.Done / pr.Total;
+                };
+
+                _vmMain.ProgressState = System.Windows.Shell.TaskbarItemProgressState.Normal;
+                _vmMain.ProgressValue = 0;
+                foreach (var sp in spines)
+                {
+                    if (ct.IsCancellationRequested)
+                    {
+                        _logger.Info("Export cancelled");
+                        break;
+                    }
+
+                    var filename = $"{sp.Name}_{timestamp}_{Guid.NewGuid().ToString()[..6]}.psd";
+                    var output = Path.Combine(_outputDir ?? sp.AssetsDir, filename);
+
+                    if (_autoResolution) SetAutoResolutionStatic(exporter, sp);
+
+                    try
+                    {
+                        exporter.Export(output, ct, sp);
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.Debug(ex.ToString());
+                        _logger.Error("Failed to export {0}, {1}", output, ex.Message);
+                    }
+                }
+                _vmMain.ProgressState = System.Windows.Shell.TaskbarItemProgressState.None;
+            }
+        }
+    }
+}
