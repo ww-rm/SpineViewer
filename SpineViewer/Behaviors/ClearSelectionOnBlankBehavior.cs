e43280bf8edfae8a1df4e8185ee47d0d16224835
ww-rm <ww-rm@qq.com>
2026-06-28 11:17:32 +0800

    增加列表空白点击附加行为

---
+++ SpineViewer/Behaviors/ClearSelectionOnBlankBehavior.cs
@@ -0,0 +1,51 @@
+﻿using SpineViewer.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace SpineViewer.Behaviors
+{
+    public static class ClearSelectionOnBlankBehavior
+    {
+        public static readonly DependencyProperty EnableProperty = DependencyProperty.RegisterAttached(
+            "Enable",
+            typeof(bool),
+            typeof(ClearSelectionOnBlankBehavior),
+            new PropertyMetadata(false, OnEnableChanged)
+        );
+
+        public static bool GetEnable(DependencyObject obj) => (bool)obj.GetValue(EnableProperty);
+
+        public static void SetEnable(DependencyObject obj, bool value) => obj.SetValue(EnableProperty, value);
+
+        private static void OnEnableChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is not ListBox list)
+                return;
+
+            if ((bool)e.NewValue)
+                list.MouseLeftButtonDown += OnMouseLeftButtonDown;
+            else
+                list.MouseLeftButtonDown -= OnMouseLeftButtonDown;
+        }
+
+        private static void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var list = (ListBox)sender;
+
+            if (e.OriginalSource is not DependencyObject source)
+                return;
+
+            // 点击到了一个 ListBoxItem，不处理
+            if (source.GetParent<ListBoxItem>(true) != null)
+                return;
+
+            list.UnselectAll();
+        }
+    }
+}
--- SpineViewer/Resources/Theme.xaml.cs
+++ SpineViewer/Resources/Theme.xaml.cs
@@ -12,18 +12,6 @@ namespace SpineViewer.Resources
 {
     public partial class Theme : ResourceDictionary
     {
-        private void ListView_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            var list = (ListView)sender;
-            if (((DependencyObject)e.OriginalSource)?.GetParent<ListViewItem>(true) is null)
-                list.SelectedItems.Clear();
-        }
 
-        private void ListBox_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            var list = (ListBox)sender;
-            if (((DependencyObject)e.OriginalSource)?.GetParent<ListBoxItem>(true) is null)
-                list.SelectedItems.Clear();
-        }
     }
 }
