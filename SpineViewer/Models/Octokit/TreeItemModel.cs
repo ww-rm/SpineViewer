6e0bd079cb38571a8086cf5a6ea902a167b84ed1
ww-rm <ww-rm@qq.com>
2026-08-22 20:42:36 +0800

    增加响应对象模型

---
+++ SpineViewer/Models/Octokit/TreeItemModel.cs
@@ -0,0 +1,37 @@
+﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpineViewer.Models.Octokit
+{
+    public class TreeItemModel
+    {
+        public TreeItemModel() { }
+
+        public TreeItemModel(TreeItem treeItem)
+        {
+            Path = treeItem.Path;
+            Mode = treeItem.Mode;
+            Type = treeItem.Type.Value;
+            Size = treeItem.Size;
+            Sha = treeItem.Sha;
+            Url = treeItem.Url;
+        }
+
+        public string? Path { get; set; }
+
+        public string? Mode { get; set; }
+
+        public TreeType Type { get; set; }
+
+        public int Size { get; set; }
+
+        public string? Sha { get; set; }
+
+        public string? Url { get; set; }
+    }
+}
---
+++ SpineViewer/Models/Octokit/TreeResponseModel.cs
@@ -0,0 +1,30 @@
+﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpineViewer.Models.Octokit
+{
+    public class TreeResponseModel
+    {
+        public TreeResponseModel() { }
+
+        public TreeResponseModel(TreeResponse treeResponse)
+        {
+            Sha = treeResponse.Sha;
+            Url = treeResponse.Url;
+            Tree = treeResponse.Tree.Select(v => new TreeItemModel(v)).ToList();
+            Truncated = treeResponse.Truncated;
+        }
+
+        public string? Sha { get; set; }
+
+        public string? Url { get; set; }
+
+        public List<TreeItemModel>? Tree { get; set; }
+
+        public bool Truncated { get; set; }
+    }
+}
